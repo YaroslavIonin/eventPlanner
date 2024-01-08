e7e59ca5deb233b5f34bbd902702af21490e17c8
YaroslavIonin <ioninyaroslav22@gmail.com>
2024-01-08 23:32:57 +0500

    Update filters for events

--- backend/server/apps/events/filtersets/event.py
+++ backend/server/apps/events/filtersets/event.py
@@ -11,16 +11,28 @@ class EventFilterSet(filters.FilterSet):
     )
     month = DateFilter(
         field_name='event_date',
-        method='custom_filter_date',
+        method='custom_filter_date_by_month',
+    )
+    week = DateFilter(
+        field_name='event_date',
+        method='custom_filter_date_by_week',
     )
 
     class Meta:
         model = Event
         fields = ['date', 'month', 'child']
 
-    def custom_filter_date(self, queryset: QuerySet, name, value):
+    def custom_filter_date_by_month(self, queryset: QuerySet, name, value):
         queryset = queryset.filter(
             event_date__month=value.month,
             event_date__year=value.year,
+        )
+        return queryset
+
+    def custom_filter_date_by_week(self, queryset: QuerySet, name, value):
+        year, week, _ = value.isocalendar()
+        queryset = queryset.filter(
+            event_date__week=week,
+            event_date__year=year,
         )
         return queryset
