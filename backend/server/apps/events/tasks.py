7b6e8010f74b3710ab54ff4e88b511e013918093
YaroslavIonin <ioninyaroslav22@gmail.com>
2024-01-18 17:55:26 +0500

    fix bugs

--- backend/server/apps/events/tasks.py
+++ backend/server/apps/events/tasks.py
@@ -6,8 +6,6 @@ from apps.events.models import Event
 @app.task()
 def push_notification(pk):
     event = Event.objects.get(pk=pk)
-    event.event_description = 'Я изменился'
-    event.save()
     print(event.id)
     print(event.main_name)
     print(event.event_date)
